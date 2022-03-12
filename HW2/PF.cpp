d236e2c151af72154176dabb785813e3d8b50425
Pin-Shao Chen <hardymike6308@gmail.com>
2022-03-12 22:33:14 +0800

    HW2

--- HW2/PF.cpp
+++ HW2/PF.cpp
@@ -13,17 +13,29 @@ int main(){
 	for(int i = 0; i < n; i++){
 		int bottom = h[i];
 		while(st.size() > 0){
+			
 			pair<int, int> last = st.top();
 			st.pop();
+			
+			cout << "bottom:" << bottom << '\n';
+			cout << "last.first :" << last.first  << '\n';
+			cout << "last.second :" << last.second  << '\n';
+			cout << "h[i]:" << h[i] << '\n';
 			bottom = min(last.second, bottom);
+			
+			cout << "bottom:" << bottom << '\n';
+			
 			if(last.first > h[i]){
 				st.push(make_pair(last.first, bottom));
 				ans = max(ans, h[i] - bottom);
+				cout << ans << '\n';
 				break;
 			}
 			ans = max(ans, last.first - bottom);
+			cout << ans << '\n';
 		}
 		st.push(make_pair(h[i], h[i]));
+		cout << "h[i] x" << h[i] << '\n';
 	}
 	
 	cout << ans << '\n';
